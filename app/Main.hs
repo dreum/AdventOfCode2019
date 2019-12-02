a321104b6a224958053102a1d274b9a5447757f9
Dirk Reum <reumdirkd@johndeere.com>
2019-12-02 14:33:01 -0600

    pull day selection out, handle invalid args

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,27 @@
 module Main where
 
 import Day1
+import System.IO
+import System.Exit
+import System.Environment
+import Data.Char
+
+switchOnDay :: String -> [Integer] -> String
+switchOnDay day
+    | (fmap toLower day) == "day1" = runDay1
+    | otherwise = (\_ -> "invalid input: please include a valid day you want to run")
 
 main :: IO ()
 main = do
-        contents <- getContents
-        let allLines = lines contents
-        putStrLn $ runDay1 $ fmap read allLines
+    args <- getArgs
+    case args of 
+        [dayArgument] -> do   
+            contents <- getContents
+            let allLines = fmap read $ lines contents
+            let runner = switchOnDay dayArgument
+            putStrLn $ runner allLines
+        _ -> do
+            name <- getProgName
+            hPutStrLn stderr $ "usage: " ++ name ++ " dayN"
+            exitFailure
+
