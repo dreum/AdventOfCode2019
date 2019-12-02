dc1ca5003db75dff6fbab8d542dd8d8e10bb5587
Dirk Reum <reumdirkd@johndeere.com>
2019-12-02 12:52:20 -0600

    improved test names

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,17 +4,17 @@ import Day1
 main :: IO ()
 main = hspec $ do 
     describe "Day1 tests" $ do
-        it "calculates fuel required 1" $ do
+        it "given mass, fuel required is mass / 3 - 2" $ do
             fuelRequired 12 `shouldBe` (2 :: Integer)
-        it "calculates fuel required 2" $ do
+        it "fuelRequired handles rounding integers down" $ do
             fuelRequired 14 `shouldBe` (2 :: Integer)
-        it "calculates fuel required 3" $ do
+        it "fuelRequired extra test 1" $ do
             fuelRequired 1969 `shouldBe` (654 :: Integer)
-        it "calculates fuel required 4" $ do
+        it "fuelRequired extra test 2" $ do
             fuelRequired 100756 `shouldBe` (33583 :: Integer)
-        it "calculates total fuel required 1" $ do
+        it "given that the fuel mass is small, no additional fuel is required for the total" $ do
             totalFuelRequired 14 `shouldBe` (2 :: Integer)
-        it "calculates total fuel required 2" $ do
+        it "totalFuelRequired takes fuel mass into account" $ do
             totalFuelRequired 1969 `shouldBe` (966 :: Integer)
-        it "calculates total fuel required 3" $ do
+        it "totalFuelRequired extra test 1" $ do
             totalFuelRequired 100756 `shouldBe` (50346 :: Integer)
