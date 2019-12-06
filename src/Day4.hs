82e50d6070d7a193b4468cc123ca156276d26051
Dirk Reum <reumdirkd@johndeere.com>
2019-12-05 21:21:15 -0600

    part 1 done

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,7 +1,27 @@
 module Day4 
-    ( 
+    ( hasDuplicate,
+      nonDecreasing,
+      parseDigits,
       runDay4
     ) where
 
+import Control.Monad
 
-runDay4  = "Day4 part 1:\n"  
+listToPairs :: [a] -> [(a,a)]
+listToPairs [] = []
+listToPairs (x:[]) = []
+listToPairs (x:y:xs) = (x,y) : listToPairs (y:xs)
+
+hasDuplicate :: [Int] -> Bool
+hasDuplicate xs = any (uncurry (==)) $ listToPairs xs
+
+nonDecreasing :: [Int] -> Bool
+nonDecreasing xs = all (uncurry (<=)) $ listToPairs xs
+
+charToString c = [c]
+
+parseDigits :: Int -> [Int]
+parseDigits s = map (read . charToString) $ show s
+
+runDay4  = "Day4 part 1:\n" ++
+           (show $ length $ filter (liftM2 (&&) hasDuplicate nonDecreasing) $ map parseDigits [240920..789857])
--- test/Day4Spec.hs
+++ test/Day4Spec.hs
@@ -4,6 +4,18 @@ import Test.Hspec
 import Day4
 
 spec = do 
-    describe "Day4" $ do
-        it "failing test" $ do
-            False `shouldBe` True
+    describe "Duplicates" $ do
+        it "reports true for duplicates" $ do
+            hasDuplicate [1,1] `shouldBe` True
+    describe "Duplicates" $ do
+        it "reports false for non-duplicates" $ do
+            hasDuplicate [1,2] `shouldBe` False
+    describe "Non decreasing" $ do
+        it "reports true for increasing numbers" $ do
+            nonDecreasing [1,2] `shouldBe` True
+    describe "Non decreasing" $ do
+        it "reports false for decreasing numbers" $ do
+            nonDecreasing [2,1] `shouldBe` False
+    describe "parsing" $ do
+        it "can parse correctly" $ do
+            (parseDigits 523456) `shouldBe` [5,2,3,4,5,6] 
