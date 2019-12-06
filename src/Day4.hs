6edd952ebe61ee722eaf01709ff64fdc9589bac3
Dirk Reum <reumdirkd@johndeere.com>
2019-12-05 19:52:05 -0600

    first commit for day4

---
+++ src/Day4.hs
@@ -0,0 +1,7 @@
+module Day4 
+    ( 
+      runDay4
+    ) where
+
+
+runDay4  = "Day4 part 1:\n"  
---
+++ test/Day4Spec.hs
@@ -0,0 +1,9 @@
+module Day4Spec (spec) where
+
+import Test.Hspec
+import Day4
+
+spec = do 
+    describe "Day4" $ do
+        it "failing test" $ do
+            False `shouldBe` True
